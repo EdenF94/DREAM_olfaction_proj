632470615bb839bf12e39dc1506513a7e6607a58
edenfeldman <eden1994@gmail.com>
2018-11-08 12:03:59 -0500

    new batch commands

--- RFandHier.sh
+++ RFandHier.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH -p gpu8_medium
-#SBATCH --nodes=1
+#SBATCH --partition=gpu8_medium
+#SBATCH --nodes=2
 #SBATCH --tasks-per-node=16
 #SBATCH --cpus-per-task=1
-#SBATCH --time=2-00:00:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH --time=12:00:00
+#SBATCH --mem=4G
 #SBATCH --gres=gpu:8
+#SBATCH --output=olfaction %j.log
 
 module load r/3.5.0
 Rscript RFandHier.R

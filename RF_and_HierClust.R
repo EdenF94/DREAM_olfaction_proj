bf3dcb3ae5eeb21a4f6e45506b2ef3880ec96313
edenfeldman <eden1994@gmail.com>
2018-11-10 14:56:53 -0500

    Created a Valence Predictor (for RF)

--- RF_and_HierClust.R
+++ RF_and_HierClust.R
@@ -10,9 +10,11 @@ total_olfac <- read.csv("Olfaction_Merged.csv", stringsAsFactors = FALSE)
 # Create Factors for labels
 total_olfac$INTENSITY.STRENGTH <- factor(total_olfac$INTENSITY.STRENGTH)
 total_olfac$VALENCE.PLEASANTNESS <- factor(total_olfac$VALENCE.PLEASANTNESS)
+as.tibble(total_olfac)
 
 # Intensity df
 olfac_intensity <- subset(total_olfac, select = -c(1,2,3,4))
+olfac_valence <- subset(total_olfac, select= -c(1,2,3,4))
 
 # Standardize features
 # olfac_intensity <- data.frame(scale(olfac_intensity))
@@ -20,6 +22,11 @@ olfac_intensity <- subset(total_olfac, select = -c(1,2,3,4))
 # Add intensity labels and move to first column
 olfac_intensity$INTENSITY.STRENGTH <- total_olfac$INTENSITY.STRENGTH
 olfac_intensity <- olfac_intensity %>% select(INTENSITY.STRENGTH, everything())
+olfac_valence$VALENCE.PLEASANTNESS <- total_olfac$VALENCE.PLEASANTNESS
+olfac_valence <- olfac_valence %>% select(VALENCE.PLEASANTNESS, everything())
+
+# remove NAs from Valence
+olfac_valence <- na.omit(olfac_valence) ## about 15,000 observations
 
 # Clean environment
 rm(total_olfac)
@@ -27,6 +34,8 @@ rm(total_olfac)
 # Set seed
 set.seed(1000009)
 
+
+### PREDICTING INTENSITY/STRENGTH ###
 # Create train and test set
 train_size <- floor(0.75 * nrow(olfac_intensity))
 train_pos <- sample(seq_len(nrow(olfac_intensity)), size = train_size)
@@ -64,3 +73,39 @@ RF_pred <- predict(RF_classification, newdata=test_set)
 # Make confusion matrix
 confusionMatrix(RF_pred, reference = test_set$INTENSITY.STRENGTH)
 
+
+### VALENCE/PLEASANTNESS ###
+train_size <- floor(0.75 * nrow(olfac_valence))
+train_pos <- sample(seq_len(nrow(olfac_valence)), size = train_size)
+
+train_set <- olfac_valence[train_pos,]
+test_set <- olfac_valence[-train_pos,]
+
+# Setting the VALENCE.PLEASANTNESS as a factor
+train_set$VALENCE.PLEASANTNESS <- as.factor(train_set$VALENCE.PLEASANTNESS)
+
+# Hierarchical Clustering
+hier_dist <- dist(olfac_valence[c(-1)], method = "euclidean")
+hier_clust <- hclust(hier_dist, method = "average")
+hier_clust
+plot(hier_clust)
+dev.copy(png,'HierClust.png')
+dev.off()
+
+# Random Forest
+RF_classification <- randomForest(VALENCE.PLEASANTNESS ~ ., data = train_set[1:50,], importance = TRUE, oob.times = 15, confusion = TRUE)
+RF_classification
+
+# Plot error based on number of trees
+plot(RF_classification$err.rate[,1], type = "l", ylab = "Error rate", xlab = "Number of trees", main = "Random Forest: Error Rate vs Number of Trees")
+dev.copy(png,'RandomForest.png')
+dev.off()
+
+# View importance
+importance(RF_classification)
+
+# Predict
+RF_pred <- predict(RF_classification, newdata=test_set)
+
+# Make confusion matrix
+confusionMatrix(table(RF_pred, reference = test_set$VALENCE.PLEASANTNESS))
